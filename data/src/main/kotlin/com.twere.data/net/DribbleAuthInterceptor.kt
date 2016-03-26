85be82d4ebb71ae28098fa4c27ad50a4877e5d56
shtankoksu <shtankoksu@gmail.com>
2016-03-27 01:50:56 +0200

    add DribbleAuthInterceptor for dribble auth

---
+++ data/src/main/kotlin/com.twere.data/net/DribbleAuthInterceptor.kt
@@ -0,0 +1,15 @@
+package com.twere.data.net
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class DribbleAuthInterceptor(val token: String) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain?): Response? {
+        val request: Request = chain?.request()!!.newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+        return chain?.proceed(request)
+    }
+}
