3db7ac0453f3ff3a1beec489604459c1a7446fab
shtankoksu <shtankoksu@gmail.com>
2016-03-29 13:59:10 +0300

    add DribbleAuthInterceptorTest

--- data/src/main/kotlin/com.twere.data/net/DribbleAuthInterceptor.kt
+++ data/src/main/kotlin/com.twere.data/net/DribbleAuthInterceptor.kt
@@ -4,7 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
-class DribbleAuthInterceptor(val token: String) : Interceptor {
+class DribbleAuthInterceptor(var token: String) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain?): Response? {
         val request: Request = chain?.request()!!.newBuilder()
