205f2d311d7f5925a592396e4cc84abcad7e63af
adamluissean <adamluissean@gmail.com>
2016-04-17 01:13:09 +0300

    added crashlitics

--- presentation/src/main/kotlin/com/twere/android/clean/way/MainApp.kt
+++ presentation/src/main/kotlin/com/twere/android/clean/way/MainApp.kt
@@ -1,12 +1,16 @@
 package com.twere.android.clean.way
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.twere.android.clean.way.di.component.ApplicationComponent
 import com.twere.android.clean.way.di.component.DaggerApplicationComponent
+import com.twere.presentation.BuildConfig
+import io.fabric.sdk.android.Fabric
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
-class MainApp : Application() {
+final class MainApp : Application() {
 
   companion object {
     @JvmStatic lateinit var instance: MainApp
@@ -15,9 +19,16 @@ class MainApp : Application() {
 
   override fun onCreate() {
     super.onCreate()
+    initCrashlytics()
     instance = this
     setupMainComponent()
     realmConfig()
+  }
+
+  fun initCrashlytics() {
+    var crashlyticsKit: Crashlytics = Crashlytics.Builder()
+        .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build()
+    Fabric.with(this, crashlyticsKit)
   }
 
   private fun setupMainComponent() {
