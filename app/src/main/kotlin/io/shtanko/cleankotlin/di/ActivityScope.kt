89ceaf888c436887ec6fda6ba67b627676276b65
Alexey <adamluissean@gmail.com>
2016-12-08 00:05:49 +0200

    Feature gradle build file(module)

--- app/src/main/kotlin/io/shtanko/cleankotlin/di/ActivityScope.kt
+++ app/src/main/kotlin/io/shtanko/cleankotlin/di/ActivityScope.kt
@@ -1,9 +1,6 @@
 package io.shtanko.cleankotlin.di
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 import javax.inject.Scope
 
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
 annotation class ActivityScope
