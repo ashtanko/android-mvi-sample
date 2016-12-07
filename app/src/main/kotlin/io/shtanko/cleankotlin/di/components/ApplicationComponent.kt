ec9f62d1726e9f8d011a8d66cae492daf1611d67
Alexey <adamluissean@gmail.com>
2016-12-07 23:54:11 +0200

    [WIP] Add MainActivity

--- app/src/main/kotlin/io/shtanko/cleankotlin/di/components/ApplicationComponent.kt
+++ app/src/main/kotlin/io/shtanko/cleankotlin/di/components/ApplicationComponent.kt
@@ -5,19 +5,19 @@ import io.shtanko.cleankotlin.di.modules.ApplicationModule
 import io.shtanko.cleankotlin.di.modules.HostModule
 import io.shtanko.cleankotlin.di.modules.NetworkModule
 import io.shtanko.cleankotlin.di.modules.RealmModule
-import io.shtanko.cleankotlin.ui.MainActivity
 import dagger.Component
+import io.shtanko.cleankotlin.MainActivity
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = arrayOf(
-    ApplicationModule::class,
-    NetworkModule::class,
-    RealmModule::class,
-    HostModule::class
+        ApplicationModule::class,
+        NetworkModule::class,
+        RealmModule::class,
+        HostModule::class
 ))
 interface ApplicationComponent {
-  fun inject(app: AppDelegate)
-  fun inject(activity: MainActivity)
-  //fun inject(fragment: MainFragment)
+    fun inject(app: AppDelegate)
+    fun inject(activity: MainActivity)
+    //fun inject(fragment: MainFragment)
 }
